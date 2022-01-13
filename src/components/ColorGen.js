c7277a7c0391fc56a44623765f5e427c6e9f126e
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-01-13 13:44:43 +0100

    functionality works, still needs styling

--- src/components/ColorGen.js
+++ src/components/ColorGen.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { StyledDiv } from './ColorGen.styled';
+import Values from 'values.js';
 
 const ColorGen = () => {
-  return <div>colorGen</div>;
+  const [color, setColor] = useState('');
+  const [colorValue, setColorvalue] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      setColorvalue(new Values(color).all(10));
+      //   console.log(colorValue);
+    } catch {
+      console.log('wtf');
+    }
+  };
+
+  useEffect(() => {
+    setColorvalue(new Values('#333999').all(10));
+  }, []);
+
+  return (
+    <StyledDiv>
+      <section className='control-section'>
+        <h2>Color Generator</h2>
+        <form action='' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='#333999'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </section>
+
+      <section className='color-section'>
+        {colorValue.map((item, index) => {
+          const { hex } = item;
+          return <SingleColor key={index} hex={hex} />;
+        })}
+      </section>
+    </StyledDiv>
+  );
+};
+
+const SingleColor = ({ hex }) => {
+  return (
+    <div className='single-color' style={{ backgroundColor: `#${hex}` }}>
+      #{hex}
+    </div>
+  );
 };
 
 export default ColorGen;
---
+++ src/components/ColorGen.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledDiv = styled.div`
+  /* .single-color {
+    width: 200px;
+    height: 200px;
+  } */
+`;
